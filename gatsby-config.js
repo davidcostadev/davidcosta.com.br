f2be542b6334f5cc0c900708f5dae579a8a968fa
David Costa <davidcostadev@gmail.com>
2019-07-23 08:41:27 -0300

    Improve gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,13 +81,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `David Costa Blog`,
+        short_name: `davidcostadev`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#5B1D98`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/dc-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
